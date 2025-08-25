b6f1582e5cc38771ce99200bdf8a819ea962e8f6
Gabriel-Vicentin <gabriel.vicentin.bonfim@gmail.com>
2025-08-25 20:29:50 -0300

    docstrings e cabeçalhos

---
+++ cadeira_preto.py
@@ -0,0 +1,8 @@
+#V1.1
+
+# EM python, o que é um cabeçalho do arquivo, para que ele serve? Qual a diferença entre um cabeçalho do arquivo para uma docstring? Para que serve uma docstring
+
+
+#Docstring: Docstrings são maneiras de comentar durante o código, normalmente feitas nos módulos, packages, crates. é Possível também ser acessada durante o período de funcionamento do código, possibilitando a criação de funções extras para prestar ajuda em relação a documentação.
+#Cabeçalho: É uma maneira de identificar o arquivo, seu criador, versão, descrição básica, contato, data de criação. É utilizado para dar um contexto geral do código.
+#A grande diferença é a forma que utilizamos ambas, docstrings utilizamos para comentar durante o código, com o intuito de deixar a execução mais limpa e clara. Já o cabeçalho é um contexto geral antes de utilizarmos o arquivo.
